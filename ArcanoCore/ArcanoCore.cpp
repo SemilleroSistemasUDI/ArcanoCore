789bc0983077bebde7c7e2fca690f68ee96409cf
John Alejandro Mantilla celis <alejandromantilla7@hotmail.com>
2015-10-03 21:51:30 -0500

    Archivos iniciales del proyecto ArcanoCore

---
+++ ArcanoCore/ArcanoCore.cpp
@@ -0,0 +1,25 @@
+#pragma region Copyright Semillero Master Digital - GIDSAW. 2015 - Presente.
+// Copyright Semillero Master Digital - GIDSAW. 2015-Presente. 
+// Todos los derechos reservados.
+// Este codigo fuente esta licenciado bajo la licencia con el estilo BSD y la puede encontrar 
+// en el archivo LICENSE en la raiz del proyecto. 
+//
+// =========================================================================================
+//
+// Copyright Semillero Master Digital - GIDSAW. 2015-Present. 
+// All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. 
+#pragma endregion
+
+// ArcanoCore.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ArcanoCore
-Proyecto: Estudio de la utilización del ancho de banda.
+
+Herramienta para procesar los logs en paralelo. Desarrollada con C++ - Consola - Win32 API - Intel Parallel Studio XE.
+
+Proyecto: Estudio de utilización del ancho de banda en las salas de informática del edificio Jorge Luis Borgues en la Universitaria de Investicación y Desarrollo (UDI).
+
+---
+
+Tool for processing logs  in parallel. Developed with C++ - Consola - Win32 API - Intel Parallel Studio XE.
+
+Project: Study of utilization of the bandwidth in the computer rooms of the Jorge Luis Borgues building in the Universitaria de Investicación y Desarrollo (UDI).
+
+
+### Traducciones ###
+
++ English
